294e3f11d8eb95aa3f44658e4eb75d62994de142
JasonVonKrueger <rpoore@gmail.com>
2022-05-21 18:20:04 -0400

    updates

--- resources/js/elements.js
+++ resources/js/elements.js
@@ -1,17 +1,20 @@
 /* global vars */
 
 module.exports = function() { 
-    const __volumeControl = document.querySelector('#volControl')
-    const __gameBoard = document.querySelector('#gameboard')
+    const sndClick = new Sound('resources/sounds/click.mp3'),
+          sndOpeningTrack = new Sound('resources/sounds/cinematic-soundtrack-plain-chant-12502.mp3'),
+          sndBackgroundMusic = new Sound('resources/sounds/davinci-music.mp3'),
+          sndSymbolFormed = new Sound('resources/sounds/symbol-formed.mp3'),
+          sndDroppingPieces = new Sound('resources/sounds/dropping-pieces.mp3'),
+          sndPickPiece = new Sound('resources/sounds/pickpiece.mp3')
 
-    let __gameID = null
-    let __playerID = null
-    let __playerName = null
-    let __currentPlayer = null
-    let __activeGamePiece = null
-    let __availableSlots = []
-
-
+    const GAME = new Game()
+    const ME = {}
+    
+    const FADE_DUR = 700, 
+          MIN_DUR = 4000
+    
+    let toastContain = null
 }
 
 
